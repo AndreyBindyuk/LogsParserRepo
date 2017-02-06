63781776ed0f1c6fc32998316d55d888ac194398
Andrey_Bindyuk <Andrey_Bindyuk@epam.com>
2017-02-06 17:40:55 +0600

    logic by search logs by project folder has been added

---
+++ src/main/java/parser/LogParserByLine.java
@@ -0,0 +1,81 @@
+package parser;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Created by Andrey_Bindyuk on 2/6/2017.
+ */
+public class LogParserByLine {
+
+    List<String> nieai = new ArrayList<>();
+    List<String> common = new ArrayList<>();
+    StringBuilder listString = new StringBuilder();
+    private final String XMLVERSION = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+    private final String NIEAISERVICES = "NIEAIServices";
+    private final String NISERVICES = "NIServices";
+    private final String UFXMSG = "UFXMsg";
+
+    public List<String> parseLogsFileByLine(File file, String tagName) {
+        String starttag = Objects.equals(tagName, "UFXMsg") ? "<" + tagName : "<" + tagName + ">";
+        String endNtag = "</" + tagName + ">";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            String coline;
+            while ((line = br.readLine()) != null) {
+                coline = line.startsWith(XMLVERSION) ? line.substring(XMLVERSION.length(), line.length()).trim() : line.trim();
+                if (coline.startsWith(starttag) && coline.endsWith(endNtag)) {
+                    nieai.add(coline.substring(0, coline.length()));
+                } else if (coline.startsWith(starttag) && !coline.endsWith(endNtag)) {
+                    common.add(coline);
+                    while (((coline = br.readLine()) != null) && !coline.endsWith(endNtag)) {
+                        common.add(coline);
+                    }
+                    for (String s : common) {
+                        if (starttag.contains(NIEAISERVICES) || starttag.contains(NISERVICES)) {
+                            if (!s.equals("    </body>")) {
+                                listString.append(s);
+                            } else {
+                                nieai.add(String.valueOf(listString + "</body>" + endNtag).replaceAll("\\s", ""));
+                            }
+                        } else if (starttag.contains(UFXMSG)) {
+                            if (!Objects.equals(s, common.get(common.size() - 1))) {
+                                listString.append(s);
+                            } else {
+                                nieai.add(String.valueOf(listString + "</MsgData>" + endNtag).replaceAll("\\s", ""));
+                            }
+                        }
+                    }
+                    common.clear();
+                    listString.setLength(0);
+                } else if (!coline.startsWith(starttag) && coline.endsWith(starttag)) {
+                    while (((coline = br.readLine()) != null) && !coline.endsWith(" - [?:log:?] ")) {
+                        common.add(coline);
+                    }
+                    for (String s : common) {
+                        if (!s.equals("    </body> ")) {
+                            listString.append(s);
+                        } else {
+                            nieai.add(String.valueOf(starttag + listString + "</body>" + endNtag).replaceAll("\\s", ""));
+                        }
+                    }
+                    common.clear();
+                    listString.setLength(0);
+                } else if (!coline.startsWith(starttag) && coline.contains(XMLVERSION)) {
+                    String xmlVerSub = coline.substring(coline.indexOf(XMLVERSION), coline.length());
+                    nieai.add(xmlVerSub.substring(XMLVERSION.length(), xmlVerSub.length()));
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return nieai;
+    }
+}
--- src/main/java/services/FileService.java
+++ src/main/java/services/FileService.java
@@ -1,7 +1,5 @@
 package services;
 
-import entities.FileInfo;
-import entities.ServerInfo;
 
 import java.io.*;
 import java.nio.file.Files;
@@ -9,24 +7,14 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Stream;
+import parser.LogParserByLine;
 
 public class FileService {
-
     private String resourceName = "config.properties";
 
-//    public String getFile(FileInfo fileInfo) throws IOException {
-////        File fullFileInfo = new File(fileInfo.getDomainName() + "\\" + fileInfo.getProjectName() +"\\"+ fileInfo.getLogType());
-//
-//        FileInputStream fis = new FileInputStream(fullFileInfo);
-//        byte[] data = new byte[(int) fullFileInfo.length()];
-//        fis.read(data);
-//        fis.close();
-//        return new String(data);
-//    }
-
-    public List getDirectoriesAndFiles(String serverName){
+    public List getDirectoriesAndFiles(String serverName) {
         List<String> list = new ArrayList<>();
-        try(Stream<Path> paths = Files.walk(Paths.get(getlogsPathByServerName(serverName)))) {
+        try (Stream<Path> paths = Files.walk(Paths.get(getlogsPathByServerName(serverName)))) {
             paths.forEach(filePath -> {
                 if (Files.isRegularFile(filePath)) {
                     list.add(String.valueOf(filePath.toAbsolutePath()).substring(getlogsPathByServerName(serverName).length()));
@@ -38,14 +26,14 @@ public class FileService {
         return list;
     }
 
-    private String getlogsPathByServerName(String serverName){
+    private String getlogsPathByServerName(String serverName) {
         String value = "";
         ClassLoader loader = Thread.currentThread().getContextClassLoader();
         Properties props = new Properties();
-        try(InputStream resourceStream = loader.getResourceAsStream(resourceName)) {
+        try (InputStream resourceStream = loader.getResourceAsStream(resourceName)) {
             props.load(resourceStream);
             value = props.getProperty(serverName);
-        }catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return value;
@@ -60,56 +48,68 @@ public class FileService {
     }
 
 
-    public List<String> getNieaiResponseByTrackingId(String serverName,String path, String trackingId) throws IOException {
+    public List getNieaiResponseByTrackingId(String serverName, String path, String trackingId) throws IOException {
+        List list = new ArrayList<>();
+        if (getValueProperty(serverName, path).getName().contains("WARBA") && (getValueProperty(serverName, path).getName().contains("w4") || getValueProperty(serverName, path).getName().contains("way4"))) {
+            list.add(getNieaiResponseByTrackingId(new LogParserByLine().parseLogsFileByLine(getValueProperty(serverName, path), "NIEAIServices"), trackingId));
+            list.add(getNieaiResponseByTrackingId(new LogParserByLine().parseLogsFileByLine(getValueProperty(serverName, path), "UFXMsg"), trackingId));
+        } else if (getValueProperty(serverName, path).getName().contains("WARBA") && (getValueProperty(serverName, path).getName().contains("tcc") || getValueProperty(serverName, path).getName().contains("adapter"))) {
+            list.add(getNieaiResponseByTrackingId(new LogParserByLine().parseLogsFileByLine(getValueProperty(serverName, path), "NIServices"), trackingId));
+        }
+        return list;//must be simplified
+
+    }
+
+    public Map<String, List> getNieaiResponseByProjectFolder(String serverName, String serverPath, String trackingId) throws IOException {
+        Map<String, List> stringListMap = new LinkedHashMap<>();
+        List strings = getFilesFromProjectDirectory(getValueProperty(serverName, serverPath).getAbsolutePath());
+        for (int i = 0; i < strings.size(); i++) {
+            List list = new ArrayList<>();
+            if (strings.get(i).toString().contains("WARBA") && (strings.get(i).toString().contains("tcc") || strings.get(i).toString().contains("adapter"))) {
+                list.add(getNieaiResponseByTrackingId(new LogParserByLine().parseLogsFileByLine(new File(strings.get(i).toString()), "NIServices"), trackingId));
+
+            } else if (strings.get(i).toString().contains("WARBA") && (strings.get(i).toString().contains("w4") || strings.get(i).toString().contains("way4"))) {
+                list.add(getNieaiResponseByTrackingId(new LogParserByLine().parseLogsFileByLine(new File(strings.get(i).toString()), "NIEAIServices"), trackingId));
+                list.add(getNieaiResponseByTrackingId(new LogParserByLine().parseLogsFileByLine(new File(strings.get(i).toString()), "UFXMsg"), trackingId));
+            }
+            stringListMap.put(strings.get(i).toString(), list);
+            stringListMap.entrySet().removeIf(entry -> Objects.equals(entry.getValue().get(0).toString(), "[]"));
+
+        }
+        return stringListMap;
+    }
+
+    public File getValueProperty(String serverName, String path) throws IOException {
         ClassLoader loader = Thread.currentThread().getContextClassLoader();
         Properties props = new Properties();
         props.load(loader.getResourceAsStream(resourceName));
-        File file = new File(props.getProperty(serverName)+path);
-        return getNieaiResponseByTrackingId(convertEachLineToString(file),trackingId);
+        return new File(props.getProperty(serverName) + path);
     }
 
-
-    private List<String> convertEachLineToString(File file){
-        List<String> nieai = new ArrayList<>();
-        List<String> common = new ArrayList<>();
-        StringBuilder listString = new StringBuilder();
-        String startNieai = "<NIEAIServices>";
-        String endNieai = "</NIEAIServices>";
-
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                if(line.startsWith(startNieai) && line.endsWith(endNieai)){
-                    nieai.add(line.substring(0, line.length()));
-                }else if(line.startsWith(startNieai)  && !line.endsWith(endNieai)){
-                    while (((line = br.readLine()) != null) && !line.endsWith(endNieai)){
-                        common.add(line);
-                    }
-                    for(String s: common){
-                        if(!s.equals("    </body>")){
-                            listString.append(s);
-                        }else {
-                            nieai.add(String.valueOf(startNieai+listString+"</body>"+endNieai).replaceAll("\\s",""));
-                        }
-                    }
-                    common.clear();
-                    listString.setLength(0);
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return nieai;
-    }
-
-    private List<String> getNieaiResponseByTrackingId(List<String> nieaiLists, String trackingId){
+    private List<String> getNieaiResponseByTrackingId(List<String> nieaiLists, String trackingId) {
         List<String> stringList = new ArrayList<>();
-        for(String s: nieaiLists){
-            if(s.contains(trackingId)){
+        for (String s : nieaiLists) {
+            if (s.contains(trackingId)) {
                 stringList.add(s);
             }
 
         }
         return stringList;
     }
+
+    private List getFilesFromProjectDirectory(String projectPath) {
+        List list = new ArrayList<>();
+        try (Stream<Path> paths = Files.walk(Paths.get(projectPath))) {
+            paths.forEach(filePath -> {
+                if (Files.isRegularFile(filePath)) {
+                    list.add(String.valueOf(filePath));
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+
 }
