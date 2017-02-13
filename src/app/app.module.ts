91fea6132c0c09449c4c424b98929d19d1f3a578
Alexandr.li <muzikant1990@gmail.com>
2017-02-13 23:05:06 +0600

    Add material design dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,35 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpModule } from '@angular/http';
-import { FormsModule } from '@angular/forms';
-import { AccordionModule } from 'ng2-bootstrap';
-import { AppComponent } from './app.component';
-import { LogsDataComponent } from './logs-data.component';
-import { FileTreeComponent } from './file-tree.component';
-import { SpinnerComponent } from './spinner.component';
-import { RestService } from './rest.service';
-import { Parser } from './parser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpModule} from '@angular/http';
+import {FormsModule} from '@angular/forms';
+import {AccordionModule} from 'ng2-bootstrap';
+import {AppComponent} from './app.component';
+import {LogsDataComponent} from './logs-data.component';
+import {FileTreeComponent} from './file-tree.component';
+import {SpinnerComponent} from './spinner.component';
+import {RestService} from './rest.service';
+import {Parser} from './parser';
+import {MaterialModule} from '@angular/material';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    HttpModule,
-    FormsModule,
-    AccordionModule.forRoot()
-  ],
-  declarations: [
-    AppComponent,
-    LogsDataComponent,
-    FileTreeComponent,
-    SpinnerComponent
-  ],
-  providers: [
-    RestService,
-    Parser
-  ],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        HttpModule,
+        FormsModule,
+        MaterialModule.forRoot(),
+        AccordionModule.forRoot()
+    ],
+    declarations: [
+        AppComponent,
+        LogsDataComponent,
+        FileTreeComponent,
+        SpinnerComponent
+    ],
+    providers: [
+        RestService,
+        Parser
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 }
