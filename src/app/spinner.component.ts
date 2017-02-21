338bfaeff95ad73a9f8422fd9d7f176e8fed3662
Alexandr_Li <Alexandr_li@epam.com>
2017-02-21 20:12:42 +0600

    add spinner size directive

--- src/app/spinner.component.ts
+++ src/app/spinner.component.ts
@@ -2,8 +2,9 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'spinner',
-  template: '<i *ngIf="visible" class="fa fa-spinner fa-spin fa-3x"></i>'
+  template: '<i *ngIf="visible" class="fa fa-spinner fa-spin fa-{{size}}"></i>'
 })
 export class SpinnerComponent {
   @Input() visible = true;
+  @Input() size = '3x';
 }
